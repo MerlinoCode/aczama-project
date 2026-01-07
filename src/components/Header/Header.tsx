1be74ce3f681528d8ac9c744d9be5bb7de16dbdb
Jonathan Merlino <jonathan.leonel.merlino@gmail.com>
2026-01-06 23:34:17 -0300

    Add Sass support and refactor Header component styles
    - Added Sass as a dependency in package.json and package-lock.json.
    - Refactored styles in Header.scss to use variables from _variables.scss.
    - Updated _variables.scss to include color and size variables for better maintainability.
    - Enhanced Header.tsx to include a toggle button for the navigation menu.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import './Header.scss';
 
 const Header: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header className="header">
             <div className="header-container">
                 <h1 className="logo"><a href="/">Teresa Aczama</a></h1>
-                <nav className="nav">
+                <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
                     <ul>
                         <li><a href="/about">Acerca de</a></li>
                         <li><a href="/about">Libros publicados</a></li>
                         <li><a href="/contact">Contacto</a></li>
                     </ul>
                 </nav>
+                <button className={`header__toggle ${isMenuOpen ? "open" : ""}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <span></span>
+                </button>
             </div>
         </header>
     );
